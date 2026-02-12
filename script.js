4aac2c1ef285ad3b1ee3479c24822107d02273f8
nikhil941106-cloud <nikhil941106@gmail.com>
2026-02-12 14:29:42 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Happy Hug Day ❤️</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="home-screen">
+  <h1>Happy Hug Day ❤️</h1>
+  <p>
+    Tum meri life ka sabse beautiful hissa ho.  
+    Tumhari ek smile mera pura din bana deti hai.  
+    Kaash main abhi tumhe hug kar pata 🤗❤️
+  </p>
+
+  <button onclick="sendHug()">Send Me a Hug</button>
+</div>
+
+<script>
+function sendHug(){
+  alert("Virtual Hug Sent 🤗❤️");
+}
+</script>
+
+</body>
+</html>
